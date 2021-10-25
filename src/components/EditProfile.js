0fe158b3a742298bb198f280c87a442249572404
unclebanks <49108377+unclebanks@users.noreply.github.com>
2021-10-24 18:45:52 -0700

    Taking a break

--- src/components/EditProfile.js
+++ src/components/EditProfile.js
@@ -21,18 +21,51 @@ function ProfileEdit(props) {
             });
     }
     function renderUserInformation(userInformation) {
+        console.log(userInformation);
+        console.log(localStorage.getItem("userID"));
         let welcomeUser = document.getElementById("profileBodyContainerRightWelcome");
         let welcomeUserData = welcomeUser.innerHTML;
-        let firstUser = document.getElementById("profileBodyContainerRightFirst");
-        let firstUserData = firstUser.innerHTML;
-        let lastUser = document.getElementById("profileBodyContainerRightLast");
-        let lastUserData = lastUser.innerHTML;
-        console.log(welcomeUser);
-        console.log(userInformation.username);
+        let firstUser = document.getElementById("profileBodyContainerRightFirstValue");
+        let lastUser = document.getElementById("profileBodyContainerRightLastValue");
+        let emailUser = document.getElementById("profileBodyContainerRightEmailValue");
+        let passwordUser = document.getElementById("profileBodyContainerRightPasswordValue");
         welcomeUser.innerHTML = welcomeUserData.concat(` ${userInformation.username}`);
-        firstUser.innerHTML = firstUserData.concat(` ${userInformation.firstName}`);
-        lastUser.innerHTML = lastUserData.concat(` ${userInformation.lastName}`);
+        firstUser.setAttribute("placeholder", `${userInformation.firstName}`);
+        lastUser.setAttribute("placeholder", `${userInformation.lastName}`);
+        emailUser.setAttribute("placeholder", `${userInformation.email}`);
+        passwordUser.setAttribute("placeholder", `${userInformation.password}`);
+        firstUser.setAttribute("value", `${userInformation.firstName}`);
+        lastUser.setAttribute("value", `${userInformation.lastName}`);
+        emailUser.setAttribute("value", `${userInformation.email}`);
     }
+    function submitEditChange() {
+        var userID = localStorage.getItem("userID")
+        var firstUser = document.getElementById("profileBodyContainerRightFirstValue");
+        var lastUser = document.getElementById("profileBodyContainerRightLastValue");
+        var emailUser = document.getElementById("profileBodyContainerRightEmailValue");
+        var passwordUser = document.getElementById("profileBodyContainerRightPasswordValue");
+        var newUserInfo = {
+            username: userID,
+            firstName: firstUser,
+            lastName: lastUser,
+            email: emailUser,
+            password: passwordUser
+        }
+        updateUser(userID, newUserInfo);
+    }
+    const updateUser = (userID, newUserInfo) => {
+        console.log(userID+"axios");
+        console.log(newUserInfo+"axios");
+        axios
+          .put(`https://dungeon-site-api.herokuapp.com/api/users/${userID}`, newUserInfo)
+          .then((res) => {
+              console.log(res);
+          })
+          .catch((err) => {
+            console.log({err});
+            alert("Line3: " + err.response);
+          });
+    };
     return (
         <div id="profileContainer">
             <NavBar />
@@ -41,27 +74,21 @@ function ProfileEdit(props) {
                     <div id="profileBodyContainerRightWelcomeContainer">
                         <span id="profileBodyContainerRightWelcome">Welcome</span>
                         <div id="editProfilePersonal">
-                            <span id="profileBodyContainerRightFirst">First Name:</span>
-                            <span id="profileBodyContainerRightLast">Last Name:</span>
-                            <span id="profileBodyContainerRightEmail">Email:</span>
-                            <span id="profileBodyContainerRightPassword">Password:</span>
+                            <span id="profileBodyContainerRightFirst">First Name: 
+                                <input id="profileBodyContainerRightFirstValue"></input>
+                            </span>
+                            <span id="profileBodyContainerRightLast">Last Name: 
+                                <input id="profileBodyContainerRightLastValue"></input>
+                            </span>
+                            <span id="profileBodyContainerRightEmail">Email: 
+                                <input id="profileBodyContainerRightEmailValue"></input>
+                            </span>
+                            <span id="profileBodyContainerRightPassword">Password: 
+                                <input id="profileBodyContainerRightPasswordValue"></input>
+                            </span>
                         </div>
-                        <div id="editProfileAddress">
-                            <span id="editProfileAddressStreet">Street: 
-                                <input id="editProfileAddressStreetValue"></input>
-                            </span>
-                            <span id="editProfileAddressApt">
-                                <input id="editProfileAddressAptValue"></input>
-                            </span>
-                            <span id="editProfileAddressCity">
-                                <input id="editProfileAddressCityValue"></input>
-                            </span>
-                            <span id="editProfileAddressState">
-                                <input id="editProfileAddressStateValue"></input>
-                            </span>
-                            <span id="editProfileAddressZip">
-                                <input id="editProfileAddressZip"></input>
-                            </span>
+                        <div id="editProfileSubmit">
+                            <button id="editProfileSubmitButton" onClick={submitEditChange}>SUBMIT</button>
                         </div>
                     </div>
                 </div>
