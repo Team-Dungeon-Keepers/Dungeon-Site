7b06fa0afeb97fefb785378e73c549514d21fe4f
tbridey <taelormcbride@icloud.com>
2021-10-24 18:38:28 -0600

    working version of gameView

--- src/components/GameView.js
+++ src/components/GameView.js
@@ -6,109 +6,127 @@ import { withRouter } from 'react-router';
 
 function GameView(){
 
-    const [game, setGame] = useState([]);
      const[name, setName] = useState();
      const[desc, setDesc] = useState();
-    //  const[gRules, setGRules] = useState();
-    //  const[addressL1, setAddressL1] = useState();
-    //  const[addressL2, setAddressL2] = useState();
-    //  const[times, setTimes] = useState();
-    //  const[starts, setStarts] = useState();
-    //  const[ends, setEnds] = useState();
-    //  const[hRules, setHRules] = useState([]);
-    //  const[lang, setLang] = useState([]);
-    //  const[gm, setGM] = useState();
-    //  const[players, setPlayers] = useState([]);
-     // const[id, setID] = useState();
+     const[gRules, setGRules] = useState();
+     const[addressL1, setAddressL1] = useState();
+     const[addressL2, setAddressL2] = useState();
+     const[times, setTimes] = useState();
+     const[starts, setStarts] = useState();
+     const[ends, setEnds] = useState();
+     const[hRules, setHRules] = useState([]);
+     const[lang, setLang] = useState([]);
+     const[gm, setGM] = useState();
+     const[players, setPlayers] = useState([]);
+     const[id, setID] = useState(sessionStorage.getItem('gameID'));
 
-    //setID(sessionStorage.getItem('gameID'));
     //console.log(id);
     //const id = 5//sessionStorage.getItem('gameID')
 
-    const gameObj = {
-        id:5,
-        name:"",
-        desc:"",
-        gRules:"",
-        addressL1:"",
-        addressL2:"",
-        times:"",
-        starts:"",
-        ends:"",
-        hRules:[],
-        lang:[],
-        gm:"",
-        players:[]
-    }
-    
-    const id = 5;
+    const testid = 5;
     useEffect(() => {
-        getGame();
-        //console.log('++++++'+gameObj.name);
-        setName(gameObj.name);
-        setDesc(gameObj.desc);
-        console.log("+++++++"+name)
-    }, )
+        //console.log();
+        //setID(sessionStorage.getItem('gameID'));
+        getGame()
+    }, [])
 
     const getGame = async () => {
         try{
             const { data } = await axios.get(`https://dungeon-site-api.herokuapp.com/api/games/full/${id}`);
-            //console.log(data.game.gameName);
-            gameObj.name = data.game.gameName;
-            gameObj.desc = data.game.description;
-            gameObj.gRules=data.rulesName;
-            gameObj.addressL1=`${data.addresses[0].street}, ${data.addresses[0].apartment}`;
-            gameObj.addressL2=`${data.addresses[0].city}, ${data.addresses[0].state}`;
-            gameObj.times=`${data.schedules[0].startTime} - ${data.schedules[0].endTime}`;
-            gameObj.starts=data.schedules[0].startDate;
-            gameObj.ends=data.schedules[0].endDate;
-            gameObj.hRules=data.behaviors;
-            gameObj.lang=data.languages;
-            gameObj.gm=data.languages;
-            gameObj.players=data.users;
-            
-            console.log('------'+gameObj.name);
-            console.log('------'+gameObj.desc);
-            console.log('------'+gameObj.gRules);
-            console.log('------'+gameObj.addressL1);
-            console.log('------'+gameObj.addressL2);
-            console.log('------'+gameObj.times);
-            console.log('------'+gameObj.starts);
-            console.log('------'+gameObj.ends);
-            console.log('------'+gameObj.hRules);
-            console.log('------'+gameObj.lang[0].language);
-            console.log('------'+gameObj.gm);
-            console.log('------'+gameObj.players);
+            console.log(data)
+            setStates(data);
+           
         }
         catch(error) {
             console.log(error)
         }
 	}
-    function setStates(data){
+    const setStates = async (data) => {
         setName(data.game.gameName);
         setDesc(data.game.description);
-        // setGRules(data.rulesName);
-        // setAddressL1(`${data.addresses[0].street}, ${data.addresses[0].apartment}`);
-        // setAddressL2(`${data.addresses[0].city}, ${data.addresses[0].state}`);
-        // setTimes(`${data.schedules[0].startTime} - ${data.schedules[0].endTime}`);
-        // setStarts(data.schedules[0].startDate);
-        // setEnds(data.schedules[0].endTime);
-        // setHRules(data.behaviors);
-        // setLang(data.languages)
-        // setGM(data.gmname);
-        // setPlayers(data.users)
+        setGRules(data.rulesName);
+        setAddressL1(`${data.addresses[0].street}, ${data.addresses[0].apartment}`);
+        setAddressL2(`${data.addresses[0].city}, ${data.addresses[0].state}`);
+        setTimes(`${data.schedules[0].startTime} - ${data.schedules[0].endTime}`);
+        setStarts(data.schedules[0].startDate);
+        setEnds(data.schedules[0].endTime);
+        setHRules(data.behaviors);
+        setLang(data.languages)
+        setGM(data.gmname);
+        setPlayers(data.users)
 
-        //console.log("set id for gameView: "+id);
+        //console.log(hRules[0]);
 
     }
+    // function printView(data){
+    //     console.log(data);
+
+    //     let name = document.getElementById("name");
+    //     let desc = document.getElementById("description");
+    //     let gRules = document.getElementById("gameRules");
+    //     let addressL1 = document.getElementById("addressL1");
+    //     let addressL2 = document.getElementById("addressL2");
+    //     let times = document.getElementById("times-p");
+    //     let starts = document.getElementById("starts-p");
+    //     let ends = document.getElementById("ends-p");
+    //     //include loop for multiple entries
+    //     let hRules = document.getElementById("houseRules");
+    //     //include loop for multiple entries
+    //     let lang = document.getElementById("languages");
+    //     let gm = document.getElementById("gameMaster");
+    //     //include loop for multiple entries
+    //     let players = document.getElementById("players");
+
+    //     name.innerHTML=data.game.gameName;
+        
+    //     desc.innerHTML=data.game.description;
+        
+    //     gRules.innerHTML=data.rulesName;
+        
+    //     var addressString1=`${data.addresses[0].street}, ${data.addresses[0].apartment} `;
+    //     //var state = JSON.stringify(data.addresses[0].state);
+    //     //var stateUpper = state.toUpperCase;
+    //     var addressString2=`${data.addresses[0].city}, ${data.addresses[0].state} `;
+    //     addressL1.innerHTML=addressString1;
+    //     addressL2.innerHTML=addressString2;
+        
+    //     times.innerHTML=`${data.schedules[0].startTime} - ${data.schedules[0].endTime}`;
+    //     starts.innerHTML=data.schedules[0].startDate;
+    //     ends.innerHTML=data.schedules[0].endDate;
+
+        
+    //     for(var i=0;i<data.behaviors.length;i++){
+    //         console.log("-----started loop");
+    //         var li=document.createElement("li");
+    //         li.innerHTML=data.behaviors[i].behavior;
+    //         hRules.appendChild(li);
+    //     }
+    //     for(var i=0;i<data.languages.length;i++){
+    //         console.log("-----started loop");
+    //         var li=document.createElement("li");
+    //         li.innerHTML=data.languages[i].language;
+    //         lang.appendChild(li);
+    //     }
+
+    //     gm.innerHTML=data.gmname;
+
+    //     for(var i=0;i<data.users.length;i++){
+    //         console.log("-----started loop");
+    //         var li=document.createElement("li");
+    //         li.innerHTML=data.users[i].username;
+    //         players.appendChild(li);
+    //     }
+        
+    // }
 
     return (
-        <div>
+        <div >
             <NavBar/>
             <div id="gameViewContainer">
+            
             <div id="gameViewBody">
                 <div id="viewFirstRow">
-                    <h1 id="name">{gameObj.name}</h1>
+                    <h1 id="name">{name}</h1>
                 </div>
                 <div id="viewSecondRow">
                     <div id="gameViewBodyRight">
@@ -121,36 +139,36 @@ function GameView(){
                         <div id="gameRules-div">
                             <h4>Game Rules:</h4>
                             <ul id="gameRules">
-                                <li>{/*gRules*/}</li>
+                                <li>{gRules}</li>
                             </ul>
                         </div>
                         <div id="address-div">
                             <h4>Takes Place:</h4>
-                            <p id="addressL1">{/*addressL1*/}</p>
-                            <p id="addressL2">{/*addressL2*/}</p>
+                            <p id="addressL1">{addressL1}</p>
+                            <p id="addressL2">{addressL2}</p>
                         </div>
                         <div id="schedule-div">
                             <h4 id="times-h">Times:</h4>
-                            <p id="times-p">{/*times*/}</p>
+                            <p id="times-p">{times}</p>
                             <h4 id="starts-h">Starts:</h4>
-                            <p id="starts-p">{/*starts*/}</p>
+                            <p id="starts-p">{starts}</p>
                             <h4 id="ends-h">Ends:</h4>
-                            <p id="ends-p">{/*ends*/}</p>
+                            <p id="ends-p">{ends}</p>
                         </div>
                         <div id="houseRules-div">
                             <h4>House Rules:</h4>
                             <ul id="houseRules">
-                                {/*hRules.map(rule => (
-                                    <li key={rule.behaviorID.toString()}>{rule.behavior}</li>
-                                ))*/}
+                                {hRules.map(rule => (
+                                    <li>{rule.behavior}</li>
+                                ))}
                             </ul>
                         </div>
                         <div id="languages-div">
                             <h4>We Speak:</h4>
                             <ul id="languages">
-                                {/*lang.map(language => (
-                                    <li key={language.languageID.toString()}>{language.language}</li>
-                                ))*/}
+                                {lang.map(language => (
+                                    <li>{language.language}</li>
+                                ))}
                             </ul>
                         </div>
                     </div>
@@ -159,16 +177,16 @@ function GameView(){
                             <h4>Host:</h4>
                         </div>
                         <div>
-                            <h3 id="gameMaster">{/*gm*/}</h3>
+                            <h3 id="gameMaster">{gm}</h3>
                         </div>
                         <div id="players-div">
                             <h4>Players:</h4>
                         </div>
                         <div>
                             <ul id="players">
-                            {/*players.map(player => (
-                                    <li key={player.username.toString()}>{player.username}</li>
-                            ))*/}
+                            {players.map(player => (
+                                    <li>{player.username}</li>
+                                ))}
                             </ul>
                         </div>
                     </div>
